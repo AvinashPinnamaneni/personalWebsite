e7f3d77c6ff3c0e18116df54e335c61458c0300a
avinashPinnamaneni <a.pinnamaneni@burms.de>
2025-06-19 13:23:30 +0200

    fix: deployment issue

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Hello from Heroku!"
 
-# Heroku looks for 'app' in 'app.py'
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
