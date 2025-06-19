da949581fc769b7f53497eaefe22d63353d13151
avinashPinnamaneni <a.pinnamaneni@burms.de>
2025-06-19 16:34:11 +0200

    change to personal website

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from Heroku!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get PORT from Heroku or default to 5000
+    import os
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
